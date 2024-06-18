202effdf6d5c3ba6c9af3267aa493678622510ac
Manya <manyab485@gmail.com>
2024-06-18 13:28:22 +0530

    Update

--- src/components/Instructor.js
+++ src/components/Instructor.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Instructor from "../images/profile.png"
-
 export default function body(props) {
-  return (
-    <div className="container my-5 bg-blue">
-      <div className="container-content my-4">
-        <h1>Our Instructors</h1>
-        <p>Learn from Our Experienced and Dedicated Instructors.</p>
-        <hr />
-        <div className="d-flex">
-          <div className="left-container">
-            <img src={Instructor} alt="Instructor"  className='instructor-image'/>
-          </div>
-          <div className="Right-container">
-            <h3>Instructor Name</h3>
-            <h5>Post Instructor @TNP Officer</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid accusantium, nisi nostrum modi iste illo officiis neque? Doloribus consequatur quaerat, quis iste quae beatae veniam accusantium suscipit ducimus. Qui, unde.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+return (
+<div className="container my-5 bg-blue">
+<div className="container-content my-4">
+<h1>Our Instructors</h1>
+<p>Learn from Our Experienced and Dedicated Instructors.</p>
+<hr />
+<div className="d-flex">
+<div className="left-container">
+<img src={Instructor} alt="Instructor"  className='instructor-image'/>
+</div>
+<div className="Right-container">
+<h3>Instructor Name</h3>
+<h5>Post Instructor @TNP Officer</h5>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid accusantium, nisi nostrum modi iste illo officiis neque? Doloribus consequatur quaerat, quis iste quae beatae veniam accusantium suscipit ducimus. Qui, unde.</p>
+</div>
+</div>
+</div>
+</div>
+)
 }
